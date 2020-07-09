8ba796d9c7ab6c874b14ae5e3ba9b087210220de
diego <masnimpu@gmail.com>
2020-07-09 23:20:56 +0300

    Group method for KFDocument

--- src/utils/PSPUtils.ts
+++ src/utils/PSPUtils.ts
@@ -1,5 +1,6 @@
 import {APIFileResult} from "@/api/APIFiles";
 import {Dict} from "@/app/types";
+import KFDocument from "@/client/KFDocument";
 
 export default class PSPUtils {
 
@@ -8,6 +9,15 @@ export default class PSPUtils {
         for(const item of items){
             if(groups[item.file_type] === undefined) groups[item.file_type] = [];
             groups[item.file_type].push(item);
+        }
+        return groups;
+    }
+
+    public static group(items: KFDocument[]){
+        const groups: Dict<KFDocument[]> = {};
+        for(const item of items){
+            if(groups[item.storageName] === undefined) groups[item.storageName] = [];
+            groups[item.storageName].push(item);
         }
         return groups;
     }
