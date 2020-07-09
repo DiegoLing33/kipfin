33b9776a73d3485b6012479474b91929fb1f69fb
diego <masnimpu@gmail.com>
2020-07-09 23:18:27 +0300

    Fixed types

--- src/api/APIFiles.ts
+++ src/api/APIFiles.ts
@@ -19,10 +19,10 @@ export default class APIFiles {
      * @param userId
      * @param removed
      */
-    public async list(userId?: number|NumericString, removed?: boolean): Promise<{list: APIFileResult[]}>{
+    public async list(userId?: number | NumericString, removed?: boolean): Promise<{ list: APIFileResult[] }> {
         const args: Dict<any> = {};
-        if(userId !== undefined) args["userId"] = userId;
-        if(removed !== undefined) args["removed"] = removed;
+        if (userId !== undefined) args["userId"] = userId;
+        if (removed !== undefined) args["removed"] = removed;
         return await API.request("files.list", args);
     }
 
@@ -32,7 +32,7 @@ export default class APIFiles {
      * @param fileId
      * @param statusCode
      */
-    public async setStatus(fileId: number, statusCode: number): Promise<APIResult>{
+    public async setStatus(fileId: NumericString | number, statusCode: NumericString | number): Promise<APIResult> {
         return await API.request("files.setStatus", {fileId, statusCode});
     }
 
@@ -41,7 +41,7 @@ export default class APIFiles {
      *
      * @param fileId
      */
-    public async remove(fileId: number): Promise<APIResult>{
+    public async remove(fileId: number): Promise<APIResult> {
         return await API.request("files.remove", {fileId});
     }
 
@@ -52,7 +52,7 @@ export default class APIFiles {
      * @param storage
      * @param encrypt
      */
-    public async upload(files: Blob|Blob[], storage: string, encrypt = false): Promise<APIResult>{
+    public async upload(files: Blob | Blob[], storage: string, encrypt = false): Promise<APIResult> {
         return await API.request("files.upload", {type: storage, encrypt}, {files: files});
     }
 
@@ -63,7 +63,7 @@ export default class APIFiles {
      * @param storage
      * @param userId
      */
-    public async uploadX(files: Blob|Blob[], storage: string, userId: string): Promise<APIResult>{
+    public async uploadX(files: Blob | Blob[], storage: string, userId: string): Promise<APIResult> {
         return await API.request("files.uploadX", {type: storage, userId}, {files: files});
     }
 
