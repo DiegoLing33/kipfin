654a6ef4504f5f4f4de68530ea330f2a95b3ae14
diego <masnimpu@gmail.com>
2020-07-09 23:28:28 +0300

    Added social links to the footer

--- src/components/theme/Footer.vue
+++ src/components/theme/Footer.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="view-Footer text-muted" style="opacity: 0.4">
-        <a href="http://kip.fa.ru">
-            КОЛЛЕДЖ ИНФОРМАТИКИ И ПРОГРАММИРОВАНИЯ <br/>
-            ФИНАНСОВОГО УНИВЕРСИТЕТА ПРИ ПРАВИТЕЛЬСТВЕ РОССИЙСКОЙ ФЕДЕРАЦИИ</a> <br/>
-        All Rights Reserved &COPY; 2019
-        Портал разработан <a href="http://ling.black">Панов Я.В.</a>
-<!--        <br/>-->
-<!--        <img style="margin: 20px auto; height: 60px" alt="Лого Финансового Университета" src="../assets/logo_fa.png">-->
+    <div class="view-Footer text-muted mt-5" style="opacity: 1; text-align: justify">
+        <a target="_blank" href="https://vk.com/kip_college" class="mr-3">ВКОНТАКТЕ</a>
+        <a target="_blank" href="https://instagram.com/kip_fin" class="mr-3">Инстаграм</a>
+        <a target="_blank" href="https://fb.com/kipfincollege" class="mr-3">FACEBOOK</a>
+        <a target="_blank" href="http://kip.fa.ru" class="mr-3">KIP.FA.RU</a>
+        <div style="opacity: 0.5; text-align: left" class="mt-2">
+            <a target="_blank" href="http://kip.fa.ru">
+                КОЛЛЕДЖ ИНФОРМАТИКИ И ПРОГРАММИРОВАНИЯ <br/>
+                ФИНАНСОВОГО УНИВЕРСИТЕТА ПРИ ПРАВИТЕЛЬСТВЕ РОССИЙСКОЙ ФЕДЕРАЦИИ</a> <br/>
+            <div style="opacity: 0.5; font-size: 9px; text-align: left">
+            All Rights Reserved &COPY; 2019
+            Портал разработан <a target="_blank" href="http://ling.black">Панов Я.В.</a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,7 +26,7 @@
 </script>
 
 <style scoped>
-    .view-Footer{
+    .view-Footer {
         font-size: 10px;
         text-transform: uppercase;
     }
