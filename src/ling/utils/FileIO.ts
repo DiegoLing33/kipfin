1b88a56378f9d7b512b16780297a4723b453fe2b
diego <diegoling33@gmail.com>
2020-07-11 16:53:49 +0300

    Printing function

--- src/ling/utils/FileIO.ts
+++ src/ling/utils/FileIO.ts
@@ -8,13 +8,27 @@ export default class FileIO {
      * @param name
      * @param url
      */
-    public static requestDownloadingFile(name: string, url: string){
+    public static requestDownloadingFile(name: string, url: string) {
         const a = document.createElement("a");
         document.body.appendChild(a);
         a.href = url;
         a.download = name;
         a.click();
         window.URL.revokeObjectURL(url);
+    }
+
+    /**
+     * Requests the printing action
+     * @param url
+     * @param autoHide
+     */
+    public static requestPrinting(url: string, autoHide = true) {
+        const win = window.open(url, 'PRINT') as Window;
+        win.print();
+        if (autoHide)
+            setTimeout(() => {
+                win.close();
+            }, 5000);
     }
 
 }
