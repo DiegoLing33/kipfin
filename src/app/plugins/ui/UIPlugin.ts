7889c3098f19cd07c7ff6dcdb8f5361219b560c1
diego <masnimpu@gmail.com>
2020-07-23 00:11:21 +0300

    New account plugin

---
+++ src/app/plugins/ui/UIPlugin.ts
@@ -0,0 +1,25 @@
+import Vue from "vue";
+import {NameList} from "@/ling/types/Common";
+import UI from "@/app/plugins/ui/UI";
+
+/**
+ * The account interface
+ */
+declare module 'vue/types/vue' {
+    interface Vue {
+        $ui: typeof UI;
+        $modalShow: (name: string) => void;
+    }
+}
+
+/**
+ * THE UI PLUGIN
+ */
+export default class UIPlugin {
+    static install(vue: typeof Vue, options: NameList<any> = {}) {
+        vue.prototype.$ui = UI;
+        vue.prototype.$modalShow = (name: string) => {
+            UI.eventBus.$emit("m::" + name + "::show");
+        };
+    }
+}
