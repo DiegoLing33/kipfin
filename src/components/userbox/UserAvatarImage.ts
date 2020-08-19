5a02e3873215ce0809b236bf13b58a249c065c0e
diego <masnimpu@gmail.com>
2020-08-20 01:17:30 +0300

    New avatars with name and lastname

--- src/components/userbox/UserAvatarImage.ts
+++ src/components/userbox/UserAvatarImage.ts
@@ -1,8 +1,8 @@
 import {Component, Mixins, Prop} from "vue-property-decorator";
-import {CreateElement, RenderContext, VNodeChildren} from "vue";
+import {CreateElement} from "vue";
 import UserPropComponent from "@/components/mixins/UserPropComponent";
-import {mergeData} from "vue-functional-data-merge";
 import {VNodeData} from "vue/types/vnode";
+import {NameList} from "@/ling/types/Common";
 
 //
 //  Defaults
@@ -10,7 +10,15 @@ import {VNodeData} from "vue/types/vnode";
 export const defaultUserAvatarSize = '40px';
 export const defaultUserAvatarBorderRadius = '0';
 export const defaultUserAvatarsPath = 'http://kipfin.ru/new/files/avatar/';
-export const defaultUserAvatarNoPath = '/img/no-image.png';
+export const defaultUserAvatarNoPath = ''; //'/img/no-image.png';
+export const defaultUserAvatarColors = [
+    "rgb(164, 88, 193)",
+    "rgb(88, 193, 135)",
+    "rgb(88, 129, 193)",
+    "rgb(193, 88, 88)",
+    "rgb(102, 88, 193)",
+    "rgb(173, 88, 193)",
+]
 
 /**
  * User avatar image component
@@ -19,6 +27,7 @@ export const defaultUserAvatarNoPath = '/img/no-image.png';
 export default class UserAvatarImage extends Mixins(UserPropComponent) {
     @Prop({default: defaultUserAvatarSize}) size!: string;
     @Prop({default: defaultUserAvatarBorderRadius}) borderRadius!: string;
+    private displayTextContent = "";
 
     /**
      * Renders the element
@@ -27,22 +36,57 @@ export default class UserAvatarImage extends Mixins(UserPropComponent) {
     render(h: CreateElement) {
         const node: VNodeData = {
             staticClass: "user-avatar-image",
-            style: this.avatarStyle
+            style: this.avatarStyle,
         };
-        return h("div", node);
+        return h("div", node, this.displayTextContent);
     }
 
     /**
      * Returns the avatar style
      */
     get avatarStyle() {
-        return {
+        const css: NameList = {
             width: this.size,
             height: this.size,
+            textAlign: "center",
+            verticalAlign: "middle",
             borderRadius: this.borderRadius,
-            backgroundImage: `url(${this.avatarUrl})`,
             backgroundSize: 'cover', backgroundRepeat: 'no-repeat no-repeat'
         };
+        const avatarPath = this.avatarUrl;
+        if (avatarPath !== "") css['backgroundImage'] = `url(${avatarPath})`;
+        else {
+            const boxSize = parseInt(this.size.replace(/([px%])/, ""));
+            const textSize = boxSize * 0.4;
+            const delta = boxSize - textSize;
+
+            css["fontSize"] = textSize + "px";
+            css["lineHeight"] = 1;
+            css["paddingTop"] = (delta / 2) + "px";
+            css["fontWeight"] = "400";
+            css["color"] = "white";
+
+            this.displayTextContent = this.nameText;
+        }
+        css["backgroundColor"] = this.backgroundColor;
+
+        return css;
+    }
+
+    get backgroundColor() {
+        const userId = parseInt(this.user.userId || "0");
+        if (userId % 5 === 0) return defaultUserAvatarColors[0];
+        if (userId % 4 === 0) return defaultUserAvatarColors[1];
+        if (userId % 3 === 0) return defaultUserAvatarColors[2];
+        if (userId % 2 === 0) return defaultUserAvatarColors[3];
+        if (userId % 1 === 0) return defaultUserAvatarColors[4];
+        return defaultUserAvatarColors[5];
+    }
+
+    get nameText() {
+        const lastname = this.user.lastname || "";
+        const name = this.user.name || "";
+        return (lastname.substr(0, 1) + name.substr(0, 1)).toUpperCase();
     }
 
     /**
