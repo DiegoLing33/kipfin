b05cc67dec39ea87bf5dd7a8466da6d54eb49555
diego <masnimpu@gmail.com>
2020-09-10 01:03:36 +0300

    Print button

--- src/modules/Admin/Pages/AdminStudentGroups.vue
+++ src/modules/Admin/Pages/AdminStudentGroups.vue
@@ -8,19 +8,19 @@
         </content-placeholders>
         <b-container v-else>
             <b-modal
-                @ok="onOk"
-                id="groupBuildModel" ok-title="Печать" cancel-title="Отмена" :visible="selectedGroup !== null">
+                    @ok="onOk"
+                    id="groupBuildModel" ok-title="Печать" cancel-title="Отмена" :visible="selectedGroup !== null">
                 <template #modal-header>
                     Группа {{ selectedGroup.studentGroupTitle }}
                 </template>
                 <div class="p-3">
                     <b-form-checkbox-group
-                        v-model="printColumns"
-                        :options="options"
-                        class="mb-3"
-                        stacked
-                        value-field="item"
-                        text-field="name"
+                            v-model="printColumns"
+                            :options="options"
+                            class="mb-3"
+                            stacked
+                            value-field="item"
+                            text-field="name"
                     ></b-form-checkbox-group>
                 </div>
             </b-modal>
@@ -50,15 +50,15 @@
                 </b-col>
                 <b-col class="p-3">
                     <b-button
-                        v-b-tooltip.hover title="Просмотреть группу"
-                        @click="$router.push('/admin/groups/i/' + group.studentGroupId)">
+                            v-b-tooltip.hover title="Просмотреть группу"
+                            @click="$router.push('/admin/groups/i/' + group.studentGroupId)">
                         <b-icon-eye/>
                     </b-button>
                     <b-button
-                        @click="setGroup(group)"
-                        variant="primary"
-                        class="ml-3"
-                        v-b-tooltip.hover title="Построить макет на печать">
+                            @click="setGroup(group)"
+                            variant="primary"
+                            class="ml-3"
+                            v-b-tooltip.hover title="Построить макет на печать">
                         <b-icon-printer/>
                     </b-button>
                 </b-col>
@@ -68,64 +68,59 @@
 </template>
 
 <script lang="ts">
-import {Component, Vue} from "vue-property-decorator";
-import UserContent from "@/modules/Interface/Components/UserContent.vue";
-import API from "@/core/app/api/API";
-import FileIO from "@/core/Utils/FileIO";
-
-@Component({
-    components: {UserContent}
-})
-export default class AdminStudentGroups extends Vue {
-    protected isLoading = true;
-    protected groupsList: any[] = [];
-    protected selectedGroup = null;
-    protected printColumns = ["sid", "si", "spec", "base"];
-    protected options = [
-        {item: 'sid', name: 'ID студента'},
-        {item: 'si', name: 'Номер студенческого'},
-        {item: 'att', name: 'Номер аттестата'},
-        {item: 'spec', name: 'Специальность'},
-        {item: 'base', name: 'Основа обучения'},
-        {item: 'state', name: 'Статус кабинета'},
-        {item: 'mail', name: 'E-mail студента'},
-        {item: 'phone', name: 'Телефон студента'},
-        {item: 'parents', name: 'Представители'},
-        {item: 'reg', name: 'Регистрация'},
-        {item: 'addr_reg', name: 'Адрес регистрации'},
-        {item: 'addr_live', name: 'Адрес проживания'},
-    ];
-
-    mounted() {
-        this.update();
+    import {Component, Vue} from "vue-property-decorator";
+    import UserContent from "@/modules/Interface/Components/UserContent.vue";
+    import API from "@/core/app/api/API";
+    import FileIO from "@/core/Utils/FileIO";
+    @Component({
+        components: {UserContent}
+    })
+    export default class AdminStudentGroups extends Vue {
+        protected isLoading = true;
+        protected groupsList: any[] = [];
+        protected selectedGroup = null;
+        protected printColumns = ["sid", "si", "spec", "base"];
+        protected options = [
+            {item: 'sid', name: 'ID студента'},
+            {item: 'si', name: 'Номер студенческого'},
+            {item: 'att', name: 'Номер аттестата'},
+            {item: 'spec', name: 'Специальность'},
+            {item: 'base', name: 'Основа обучения'},
+            {item: 'state', name: 'Статус кабинета'},
+            {item: 'mail', name: 'E-mail студента'},
+            {item: 'phone', name: 'Телефон студента'},
+            {item: 'parents', name: 'Представители'},
+            {item: 'reg', name: 'Регистрация'},
+            {item: 'addr_reg', name: 'Адрес регистрации'},
+            {item: 'addr_live', name: 'Адрес проживания'},
+        ];
+        mounted() {
+            this.update();
+        }
+        setGroup(group: never) {
+            this.selectedGroup = group;
+            this.$bvModal.show("groupBuildModel");
+        }
+        onOk() {
+            const request = this.printColumns.map(value => `${value}=1`);
+            FileIO.requestPrinting(
+                `http://kipfin.ru/new/index.php?class=res&method=groupPR&p=1&groupId=` +
+                `${(this.selectedGroup as any).studentGroupId}` +
+                `&token=${this.$store.getters.accountAuthToken}&${request.join('&')}`,
+                false
+            );
+        }
+        async update() {
+            const resp = await API.users.studentGroupsList();
+            const {list} = resp;
+            this.groupsList = list;
+            this.isLoading = false;
+        }
     }
-
-    setGroup(group: never) {
-        this.selectedGroup = group;
-        this.$bvModal.show("groupBuildModel");
-    }
-
-    onOk() {
-        const request = this.printColumns.map(value => `${value}=1`);
-        FileIO.requestPrinting(
-            `http://kipfin.ru/new/index.php?class=res&method=groupPR&p=1&groupId=` +
-            `${(this.selectedGroup as any).studentGroupId}` +
-            `&token=${this.$store.getters.accountAuthToken}&${request.join('&')}`,
-            false
-        );
-    }
-
-    async update() {
-        const resp = await API.users.studentGroupsList();
-        const {list} = resp;
-        this.groupsList = list;
-        this.isLoading = false;
-    }
-}
 </script>
 
 <style scoped>
-.rw:not(:last-child) {
-    border-bottom: 1px solid #efefef
-}
+    .rw:not(:last-child) {
+        border-bottom: 1px solid #efefef
+    }
 </style>
